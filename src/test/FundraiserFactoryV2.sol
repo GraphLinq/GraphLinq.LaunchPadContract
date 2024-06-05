18462a71381c42024a133dcf5228b7bd1894ac3f
unforkableco <anton@unforkable.co>
2024-06-05 13:43:58 +0200

    adding upgradeability tests

---
+++ src/test/FundraiserFactoryV2.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+import "../Fundraiser.sol";
+import "../Vesting.sol";
+
+import "../interfaces/ICampaignFactory.sol";
+import "../interfaces/ICampaign.sol";
+
+/// @custom:oz-upgrades-from FundraiserFactory
+contract FundraiserFactoryV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    // Events
+    event FundraiserCreated(address indexed fundraiser);
+    event CampaignRegistered(uint8 id);
+
+    // State variables
+    uint8 public campaignID;
+    address constant public POSITION_MANAGER = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
+
+    mapping(uint8 => ICampaignFactory) public campaigns;
+
+    /**
+     * @dev Struct to hold parameters for the Fundraiser initialization
+     */
+    struct FundraiserParams {
+        address saleToken;
+        address raiseToken;
+        string projectName;
+        string description;
+        string websiteLink;
+        ICampaign campaign;
+        uint256 vestingStartDelta;
+        uint256 vestingDuration;
+        address positionManager;
+        IVesting vesting;
+        uint24 poolFee;
+    }
+
+    /**
+     * @dev Initializes the factory contract
+     * Sets the initial owner
+    */
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+
+    /**
+     * @dev Register a new campaign
+     * @param factory The campaign factory contract
+    */
+    function registerCampaign(ICampaignFactory factory) public onlyOwner {
+        campaigns[campaignID] = factory;
+        emit CampaignRegistered(campaignID++);
+    }
+
+    /**
+     * @dev Creates a new fundraiser
+     * @param fundraiserParams Encoded parameters for the fundraiser
+     * @param campaignParams Encoded parameters for the campaign
+     * @param campaignTypeID The ID of the campaign type to use
+     * @return The address of the created fundraiser
+    */
+    function createFundraiser(
+        bytes memory fundraiserParams,
+        bytes memory campaignParams,
+        uint8 campaignTypeID
+    ) public returns (address) {
+        (
+            string memory projectName,
+            string memory description,
+            string memory websiteLink,
+            address saleToken,
+            address raiseToken,
+            uint256 vestingStartDelta,
+            uint256 vestingDuration,
+            uint24 poolFee
+        ) = abi.decode(fundraiserParams, (string, string, string, address, address, uint256, uint256, uint24));
+
+        require(campaignTypeID < campaignID, "Invalid campaign type");
+
+        // Create a new Fundraiser instance
+        Fundraiser fundraiser = new Fundraiser(msg.sender);
+
+        // Create a new campaign using the specified campaign factory
+        ICampaign campaign = campaigns[campaignTypeID].createCampaign(address(fundraiser), campaignParams);
+
+        // Initialize the vesting contract if vesting is enabled
+        IVesting vesting = IVesting(address(0));
+        if (vestingDuration > 0) {
+            vesting = IVesting(new Vesting(saleToken, address(fundraiser)));
+        }
+
+        // Initialize the parameters struct
+        Fundraiser.FundraiserParams memory params = Fundraiser.FundraiserParams({
+            saleToken: saleToken,
+            raiseToken: raiseToken,
+            projectName: projectName,
+            description: description,
+            websiteLink: websiteLink,
+            campaign: campaign,
+            vestingStartDelta: vestingStartDelta,
+            vestingDuration: vestingDuration,
+            positionManager: POSITION_MANAGER,
+            vesting: vesting,
+            poolFee: poolFee
+        });
+
+        // Initialize the fundraiser
+        fundraiser.initialize(params);
+
+        emit FundraiserCreated(address(fundraiser));
+        return address(fundraiser);
+    }
+}
---
+++ test/UpgradeabilityTest.t.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/FundraiserFactory.sol";
+import "../src/Fundraiser.sol";
+import "../src/Vesting.sol";
+import "../src/StealthLaunchFactory.sol";
+import "../src/FairLaunchFactory.sol";
+import "../src/StealthLaunchCampaign.sol";
+import "../src/FairLaunchCampaign.sol";
+import "../src/interfaces/ICampaign.sol";
+import "../src/interfaces/ICampaignFactory.sol";
+import "../src/test/MockERC20.sol";
+import "../src/test/FundraiserFactoryV2.sol";
+import "forge-std/console.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { Upgrades } from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+contract UpgradeabilityTest is Test {
+    FundraiserFactory private factory;
+    StealthLaunchFactory private stealthFactory;
+    FairLaunchFactory private fairFactory;
+    MockERC20 private saleToken;
+    MockERC20 private raiseToken;
+    address private owner;
+    uint8 private stealthLaunchID;
+    uint8 private fairLaunchID;
+    ERC1967Proxy proxy;
+
+    // Set up the test environment before running tests
+    function setUp() public {
+        owner = vm.addr(1);
+        vm.startPrank(owner);
+
+        // Deploy the factories
+        factory = new FundraiserFactory();
+        stealthFactory = new StealthLaunchFactory();
+        fairFactory = new FairLaunchFactory();
+
+        // Deploy the proxy and initialize the contract through the proxy
+        proxy = new ERC1967Proxy(address(factory), abi.encodeCall(factory.initialize, ()));
+        factory = FundraiserFactory(address(proxy));
+        
+        // Register the campaign factories
+        stealthLaunchID = factory.campaignID();
+        factory.registerCampaign(ICampaignFactory(address(stealthFactory)));
+
+        fairLaunchID = factory.campaignID();
+        factory.registerCampaign(ICampaignFactory(address(fairFactory)));
+
+        // Deploy mock tokens
+        saleToken = new MockERC20("Sale Token", "SALE");
+        raiseToken = new MockERC20("Raise Token", "RAISE");
+
+        vm.stopPrank();
+    }
+
+    // Test the basic functionality of creating a fundraiser
+    function testCreateFundraiser() public {
+        // Prepare parameters for the fundraiser
+        bytes memory fundraiserParams = abi.encode(
+            "Test Project",
+            "This is a test project.",
+            "https://testproject.com",
+            address(saleToken),
+            address(raiseToken),
+            3600,  // Vesting starts 1 hour after finalization
+            86400, // Vesting duration of 24 hours
+            3000   // Pool fee
+        );
+
+        bytes memory campaignParams = abi.encode(
+            block.timestamp + 3600, // End time 1 hour from now
+            1000 * 10**18, // Minimum goal of 1000 tokens
+            1 * 10**18     // 1 ETH per token
+        );
+
+        // Create the fundraiser
+        address fundraiserAddress = factory.createFundraiser(fundraiserParams, campaignParams, fairLaunchID);
+
+        // Assertions
+        Fundraiser fundraiser = Fundraiser(fundraiserAddress);
+        (string memory projectName, string memory description, string memory websiteLink) = fundraiser.info();
+        assertEq(projectName, "Test Project");
+        assertEq(description, "This is a test project.");
+        assertEq(websiteLink, "https://testproject.com");
+    }
+
+    // Test the upgradeability of the FundraiserFactory contract
+    function testUpgradeability() public {
+
+        // Upgrade the proxy to the new implementation
+        vm.startPrank(owner);
+        Upgrades.upgradeProxy(address(proxy), "FundraiserFactoryV2.sol:FundraiserFactoryV2", "", owner);
+        vm.stopPrank();
+
+        // Check that the upgrade was successful by calling a function from the new implementation
+        FundraiserFactory upgradedFactory = FundraiserFactory(address(proxy));
+        assertEq(upgradedFactory.owner(), owner);
+    }
+}
